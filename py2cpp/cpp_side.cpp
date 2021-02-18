23ddea6bc0edf0ef7c9fbe1906c25b369bc1ef31
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-02-19 01:10:07 +0900

    Create cpp_side.cpp

---
+++ py2cpp/cpp_side.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+template <typename T>bool py_is(const std::vector<T>&a,const std::vector<T>&b){return&a==&b;}
+template <typename T>bool py_is(T a, T b){return a==b;}
+template <typename T, typename U>bool py_is(T a, U b){return false;}
+template <typename T>bool py_in(const std::vector<T>&a,T b){return std::find(a.begin(), a.end(), b) != a.end();}
+template <typename T, typename U>bool py_in(const std::vector<T>&a,U b){return false;}
+int main(){
+
+
+
+}
