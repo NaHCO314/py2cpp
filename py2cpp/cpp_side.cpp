50a8566b9ca85b86a426ac9fe355bfa6f09bdb8b
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-02-20 19:49:13 +0900

    Update cpp_side.cpp

--- py2cpp/cpp_side.cpp
+++ py2cpp/cpp_side.cpp
@@ -4,6 +4,11 @@ template <typename T>bool py_is(T a, T b){return a==b;}
 template <typename T, typename U>bool py_is(T a, U b){return false;}
 template <typename T>bool py_in(const std::vector<T>&a,T b){return std::find(a.begin(), a.end(), b) != a.end();}
 template <typename T, typename U>bool py_in(const std::vector<T>&a,U b){return false;}
+
+auto py_mod(auto n, auto m){
+  return ((n < 0) ^ (m < 0))? m + fmod(n, m) : fmod(n, m);
+}
+
 int main(){
 
 
